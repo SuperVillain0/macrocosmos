5eab67ec552104a1ad9ae3a653b3870d1c7447e4
Absolute Reality <ambitious.abhiram001@gmail.com>
2024-03-19 21:05:46 +0530

    Backend Complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,24 +1,62 @@
-const User = require("../Models/UserModel");
+const User = require("../models/UserModel");
 const { createSecretToken } = require("../util/secretToken");
-/* const bcrypt = require("bcryptjs"); */
+// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 module.exports.Signup = async (req, res, next) => {
   try {
     const { email, password, username, createdAt } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.json({ message: "User already exists" });
+      // success: false MIGHT BE THE PROBELM
+      return res.json({ message: "User already exists", success: false });
     }
+
     const user = await User.create({ email, password, username, createdAt });
     const token = createSecretToken(user._id);
     console.log(user);
-    res.cookie("token", token, {
-      withCredentials: true,
-      httpOnly: false
-    });
+
     res
+      .cookie("token", token, {
+        withCredentials: true,
+        httpOnly: false
+      })
       .status(201)
       .json({ message: "User signed in successfully", success: true, user });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports.Login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      // success: false MIGHT BE THE PROBELM
+      return res.json({ message: "All fields are required", success: false });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      // success: false MIGHT BE THE PROBELM
+      return res.json({ message: "Incorrect credentials", success: false });
+    }
+
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      // success: false MIGHT BE THE PROBELM
+      return res.json({ message: "Incorrect credentials", success: false });
+    }
+
+    const token = createSecretToken(user.id);
+    res
+      .cookie("token", token, {
+        withCredentials: true,
+        httpOnly: false
+      })
+      .status(201)
+      .json({ message: "User logged in successfully", success: true });
     next();
   } catch (error) {
     console.error(error);
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,6 +1,9 @@
-const { Signup } = require("../controllers/authController");
+const { Signup, Login } = require("../controllers/authController");
+const { userVerification } = require("../middlewares/authMiddleware");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
+router.post("/login", Login);
+router.post("/", userVerification);
 
 module.exports = router;
