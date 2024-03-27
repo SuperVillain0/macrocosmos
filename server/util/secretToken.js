07785ba1df52f1301eb717b31e6c60ec7bae67cb
Aniketh <yepaniketh014@gmail.com>
2024-03-27 12:47:36 +0530

    SameSite None

--- server/util/secretToken.js
+++ server/util/secretToken.js
@@ -5,6 +5,7 @@ const TOKEN_KEY = process.env.TOKEN_KEY;
 
 module.exports.createSecretToken = id => {
   return jwt.sign({ id }, TOKEN_KEY, {
-    expiresIn: 3 * 24 * 60 * 60
+    expiresIn: 3 * 24 * 60 * 60,
+    SameSite: None
   });
 };
